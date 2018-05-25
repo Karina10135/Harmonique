105ddfcfad0c0a580b21ac74f81dd66bc53b6fc0
Karina <36827712+Karina10135@users.noreply.github.com>
2018-05-25 13:51:04 +1200

    start scene

---
+++ Assets/AnimateTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateTexture : MonoBehaviour {
+
+    public float speed;
+
+    private Material mat;
+    private float offset;
+    private const string tex = "_MainTex";
+
+	void Start ()
+    {
+        mat = GetComponent<Image>().material;
+	}
+	
+	void Update ()
+    {
+        offset += Time.deltaTime * speed;
+        mat.SetTextureOffset(tex, new Vector2(offset, 0));
+	}
+}
