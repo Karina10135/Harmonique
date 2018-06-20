de624a96c5a23dc6755cd65464055758db575ad5
Karina <36827712+Karina10135@users.noreply.github.com>
2018-06-20 13:56:24 +1200

    Fade credit(part 1)

--- Assets/Components/Scripts/GatePuzzle.cs
+++ Assets/Components/Scripts/GatePuzzle.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GatePuzzle : MonoBehaviour
 {
     public GameObject confete;
     public GameObject[] trumpets;
     public int[] noteSequence;
+
+    public float fadeTimer;
+    public GameObject[] creditsPanel;
+    public float fadeSpeed;
+    int currentPanel;
+    float val;
+    bool fading;
 
     public int currentNote;
     public int noteNum;
@@ -20,6 +28,15 @@ public class GatePuzzle : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            val = 1f;
+            StartCoroutine(PanelTimer(false));
+        }
+    }
+
     public void StartSequence()
     {
         
@@ -28,9 +45,6 @@ public class GatePuzzle : MonoBehaviour
     public void CheckNote(int note)
     {
         if (complete) { return; }
-        print("Playing to gate");
-
-
         if (note != currentNote) { ResetSequence(); return; }
 
 
@@ -49,7 +63,6 @@ public class GatePuzzle : MonoBehaviour
 
     public void ResetSequence()
     {
-        print("Wrong note!");
         noteNum = 0;
         AssignNote();
     }
@@ -70,7 +83,6 @@ public class GatePuzzle : MonoBehaviour
 
     public void CompletedPuzzle()
     {
-        print("Game Complete");
         Fabric.EventManager.Instance.PostEvent("Misc/Melodygatesuccess", gameObject);
         anim.SetBool("Complete", true);
     }
@@ -88,6 +100,65 @@ public class GatePuzzle : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             noteManager.gateRelay = false;
+        }
+    }
+
+
+
+    IEnumerator PanelTimer(bool fade)
+    {
+        yield return new WaitForSeconds(1);
+        print("fade timer");
+        var c = creditsPanel[0].GetComponent<CanvasGroup>();
+
+        if (fade)
+        {
+            while (val < 1)
+            {
+                val += Time.deltaTime * fadeSpeed;
+                c.alpha = val;
+                if(val >= 1f)
+                {
+                    val = 1f;
+                    NextPanel();
+
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+        else
+        {
+            while (val > 0)
+            {
+                val -= Time.deltaTime * fadeSpeed;
+                c.alpha = val;
+                print(val);
+                if (val <= 0f)
+                {
+                    val = 0f;
+                    NextPanel();
+
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+        
+
+
+        //fade out of current panel
+        //yield return new WaitForSeconds(fadeTimer);
+        //full visible panel
+        //pause to read timer
+        //fade to next panel
+    }
+
+    public void NextPanel()
+    {
+        if(currentPanel == creditsPanel.Length)
+        {
+
         }
     }
 
