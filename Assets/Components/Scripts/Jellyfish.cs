ee3e11b4c8e52c7fad9e15ce5437db4d4b587296
Karina <36827712+Karina10135@users.noreply.github.com>
2018-07-05 13:00:56 +1200

    jellyfish

--- Assets/Components/Scripts/Jellyfish.cs
+++ Assets/Components/Scripts/Jellyfish.cs
@@ -6,9 +6,11 @@ public class Jellyfish : MonoBehaviour
 {
 
     public float lightTimer;
-    public GameObject jellyfishLight;
-    //public Material jellyfishLightMaterial;
-    //public Material jellyfishStandardMaterial;
+    public ParticleSystem jellyfishLight;
+    public GameObject[] jellyfishBody;
+    public Material jellyfishStandardMaterial;
+    public Material jellyfishLightMaterial;
+
     public bool lighted;
 
     private void Start()
@@ -18,24 +20,24 @@ public class Jellyfish : MonoBehaviour
 
     public void TriggerJellyfish()
     {
-        jellyfishLight.SetActive(true);
-        if (lighted)
-        {
-            StopAllCoroutines();
-            StartCoroutine(LightTimer());
-        }
-        else
-        {
-            StartCoroutine(LightTimer());
-        }
+        StartCoroutine(LightTimer());
+
     }
 
     IEnumerator LightTimer()
     {
         lighted = true;
+        foreach(var body in jellyfishBody)
+        {
+            body.GetComponent<MeshRenderer>().material = jellyfishLightMaterial;
+        }
+        var particle = Instantiate(jellyfishLight, gameObject.transform);
         yield return new WaitForSeconds(lightTimer);
-        jellyfishLight.SetActive(false);
         lighted = false;
+        foreach (var body in jellyfishBody)
+        {
+            body.GetComponent<MeshRenderer>().material = jellyfishStandardMaterial;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
