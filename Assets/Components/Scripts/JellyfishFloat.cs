bb3e4d772d5f46ee6b73b4f04d87f6c9ad027e19
Karina <36827712+Karina10135@users.noreply.github.com>
2018-07-27 13:28:20 +1200

    jellyfish

---
+++ Assets/Components/Scripts/JellyfishFloat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyfishFloat : MonoBehaviour
+{
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float amplitude;
+
+
+    public Vector3 tempPosition;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        tempPosition = startPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        tempPosition = transform.position;
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude + startPosition.y;
+        transform.position = tempPosition;
+    }
+}
